1df7158058d8440de86014477714da39ba883040
Schools <stephen.schools@caci.com>
2018-01-29 13:12:38 -0500

    initial commit

---
+++ src/main/java/com/example/formwork/FormworkApplication.java
@@ -0,0 +1,12 @@
+package com.example.formwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormworkApplication.class, args);
+	}
+}
